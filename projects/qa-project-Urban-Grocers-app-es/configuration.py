fc8ffad92ef3b75d42f551270d86ed3a302bd907
Karen Lozano <lozanok.wl@gmail.com>
2026-04-20 14:09:28 -0600

    Corregido

--- projects/qa-project-Urban-Grocers-app-es/configuration.py
+++ projects/qa-project-Urban-Grocers-app-es/configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = 'https://cnt-cac82467-220a-46f7-8b3e-f39aa923c216.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-6eb1cd3d-2fc8-44ff-adf5-3e60ba995f7a.containerhub.tripleten-services.com'
 DOC_PATH = '/docs/'
 CREATE_USER_PATH = "/api/v1/users/"
-KITS_PATH = "/api/v1/products/kits/"
+KITS_PATH = "/api/v1/kits/"
